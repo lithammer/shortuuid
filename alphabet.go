c7c6cdf0dcdf524b410bd85ae7dbe0ae71d3e542
Peter Renström <renstrom.peter@gmail.com>
2015-11-12 14:09:23 +0100

    Gofmt on alphabet.go

--- alphabet.go
+++ alphabet.go
@@ -35,7 +35,7 @@ func (a *alphabet) Index(t string) (int64, error) {
 			return int64(i), nil
 		}
 	}
-    return 0, fmt.Errorf("Element '%v' is not part of the alphabet", t)
+	return 0, fmt.Errorf("Element '%v' is not part of the alphabet", t)
 }
 
 // dudupe removes duplicate characters from s.
