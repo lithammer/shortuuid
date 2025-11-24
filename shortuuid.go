0e84dc6abbe6b20c561c5c1cca21a59ea56f68aa
Peter Lithammer <peter.lithammer@gmail.com>
2025-11-24 21:16:43 +0100

    Remove needless lint ignore
    `Hash.Write()` never fails according to its documentation.

--- shortuuid.go
+++ shortuuid.go
@@ -59,8 +59,8 @@ func hasPrefixCaseInsensitive(s, prefix string) bool {
 
 func hashedUUID(space uuid.UUID, data string) (u uuid.UUID) {
 	h := sha1.New()
-	h.Write(space[:])                                         //nolint:errcheck
-	h.Write(unsafe.Slice(unsafe.StringData(data), len(data))) //nolint:errcheck
+	h.Write(space[:])
+	h.Write(unsafe.Slice(unsafe.StringData(data), len(data)))
 	s := h.Sum(make([]byte, 0, sha1.Size))
 	copy(u[:], s)
 	u[6] = (u[6] & 0x0f) | uint8((5&0xf)<<4)
